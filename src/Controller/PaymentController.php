4d8ec10dc2bcec13d6ca07ae60d39e9b8b1b7460
Herold7 <makpier@gmail.com>
2024-02-05 14:34:09 +0100

    Création du controller et des views pour le paiement

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ClientRepository;
+use App\Repository\OffreRepository;
+use App\Repository\TransactionRepository;
+use App\Service\StripeService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/payment')]
+class PaymentController extends AbstractController
+{
+    #[Route('/', name: 'app_payment')]
+    public function index(
+StripeService $stripeService,
+OffreRepository $offres,
+ClientRepository $clients,
+TransactionRepository $transactions
+    ): Response
+    {
+$apiKey = $this->getParameter('STRIPE_API_KEY_SECRET'); // Clé API secrète
+$offre = $offres->findOneBy(['id' => 1]); // Offre à vendfre (titre et montant)
+$clientEmail = $clients->findOneBy(['id' => 1])->getEmail(); // 
+
+$stripeService->makePayment(
+    $apiKey,
+    $offre->getMontant(),
+    $offre->getTitre(),
+    $clientEmail
+);
+        return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+
+    #[Route('/success', name: 'payment_success')]
+    public function success(): Response 
+    {
+        return $this->render('payment/success.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+    #[Route('/cancel', name: 'payment_cancel')]
+    public function cancel(): Response 
+    {
+        return $this->render('payment/cancel.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+}
